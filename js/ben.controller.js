cd4084d3f83def8ce583186baa1985489d1df42e
benBen <benmbenjamin@gmail.com>
2016-07-04 01:51:19 -0700

    walking speed

--- js/ben.controller.js
+++ js/ben.controller.js
@@ -6,14 +6,11 @@
 
   BenController.$inject = ["projectsDataService", "$log"];
 
-
-
   function BenController(projectsDataService, $log) {
     var ben = this;
     ben.$log = $log;
     ben.message = "WELL COME ON IN"
     ben.projects = projectsDataService;
-
 
     ben.selectedProject = {};
     ben.setSelectedProject = setSelectedProject;
@@ -24,19 +21,36 @@
     ben.down = false;
     ben.walk = walk;
 
+    // ben.standUp = false;
+    // ben.standLeft = false;
+    // ben.standRight = false;
+    // ben.standDown = false;
+    // ben.stand = stand;
+
     function setSelectedProject (index) {
       ben.selectedProject = ben.projects[index];
       ben.selectedProject.index = index;
       // ben.$log.log(ben.selectedProject.index);
       // $('body').css('background', 'pink');
       // ben.$log(angular.element(document).find('body'));
-      }
+
+    ben.walk(index);
     }
 
     function walk (index) {
-      $log.log(index)
+
 
       switch (index) {
+        // case 0:
+        // case 2:
+        // case 4:
+        // case 6:
+        // case 8:
+        //   ben.up = false;
+        //   ben.down = false;
+        //   ben.left = false;
+        //   ben.right = false;
+        //   break;
         case 1:
           ben.up = true;
           ben.down = false;
@@ -45,23 +59,69 @@
           break;
         case 3:
           ben.up = false;
+          ben.down = false;
           ben.left = true;
-          ben.left = false;
           ben.right = false;
           break;
         case 5:
           ben.up = false;
-          ben.left = false;
-          ben.left = true;
-          ben.right = false;
-          break;
-        case 7:
-          ben.up = false;
-          ben.left = false;
+          ben.down = false;
           ben.left = false;
           ben.right = true;
           break;
+        case 7:
+          ben.up = false;
+          ben.down = true;
+          ben.left = false;
+          ben.right = false;
+          break;
     }
+
+    function stand (index) {
+      switch (index) {
+        case 1:
+          ben.up = true;
+          ben.down = false;
+          ben.left = false;
+          ben.right = false;
+          break
+        case 3:
+          ben.up = false
+          ben.down = false;
+          ben.left = true;
+          ben.right = false;
+          break
+        case 5:
+          ben.up = false
+          ben.down = false;
+          ben.left = false;
+          ben.right = true;
+          break
+        case 7:
+          ben.up = false
+          ben.down = true;
+          ben.left = false;
+          ben.right = false;
+          break
+
+      }
+    }
+
+    // if (index == 1) {
+    //    ben.up = true;
+    //    ben.down = false;
+    //    ben.left = false;
+    //    ben.right = false;
+    // }
+
+    // else if (index == 3) {
+    //   ben.up = false;
+    //   ben.left = false;
+    //   ben.right = false;
+    //   ben.down = true;
+    // }
   }
+
+}
 
 })();
